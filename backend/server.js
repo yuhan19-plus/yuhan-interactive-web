56345b0380f84fadee0bee3ee635687b6d99dd2c
ohjaoh <ohja1004@naver.com>
2024-08-18 23:00:38 +0900

    [오자현] 백엔드와 프론트엔드 연결(포트번호4000)

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,51 @@
 /** 파일 생성자 : 임성준
- * 여기에 백엔드 작업
- *  
- * 
- * */ 
+ *
+ * 초기 연결셋팅 : 오자현
+ * 시작방법 터미널에 node server.js 입력
+ *
+ * */
+const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
+
+// CORS 설정 추가
+app.use(cors());
+app.use(express.json()); // JSON 파싱 미들웨어
+
+// MySQL 연결 설정
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "tester",
+  password: "1234",
+  database: "test1234",
+});
+
+// 클라이언트 연결 처리
+io.on("connection", (socket) => {
+  console.log("새 클라이언트 연결됨:", socket.id);
+
+  // 클라이언트 연결 끊김 처리
+  socket.on("disconnecting", () => {
+    console.log("연결 끊어지는 중")
+  });
+
+  // 클라이언트 연결 종료 처리
+  socket.on("disconnect", () => {
+    console.log("연결 끊어짐")});
+});
+
+// 서버 시작
+server.listen(4000, () => {
+  console.log("서버가 4000번 포트에서 실행 중입니다.");
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,30 @@
 /** 파일 생성자 : 임성준
  * 임성준
  *  - 초기 루트, 로그인, 회원가입, 관리자페이지, 회원관리, 오늘의 메뉴, 게시판 라우터 설정
+ * 오자현
+ *  - 소캣컨트롤 추가
  */
-import { BrowserRouter, Link, Route, Router, Routes } from 'react-router-dom'
-import './App.css'
-import ClientIndex from './client/ClientIndex'
-import AdminIndex from './admin/AdminIndex'
-import AdminMember from './admin/components/content/AdminMember'
-import AdminFood from './admin/components/content/AdminFood'
-import AdminBoard from './admin/components/content/AdminBoard'
-import AdminMain from './admin/components/content/AdminMain'
-import MemberIndex from './client/components/member/MemberIndex'
-
+import { BrowserRouter, Link, Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import ClientIndex from "./client/ClientIndex";
+import AdminIndex from "./admin/AdminIndex";
+import AdminMember from "./admin/components/content/AdminMember";
+import AdminFood from "./admin/components/content/AdminFood";
+import AdminBoard from "./admin/components/content/AdminBoard";
+import AdminMain from "./admin/components/content/AdminMain";
+import MemberIndex from "./client/components/member/MemberIndex";
+import { TestSocketControls } from "./sockets/TestSocketControls";
 function App() {
-
   return (
     <BrowserRouter>
+      <TestSocketControls />
       <Routes>
-        <Route path='/' element={<ClientIndex />} />
-        <Route path='/login' element={<MemberIndex value='login' />} />
-        <Route path='/join' element={<MemberIndex value='join' />} />
-
-        <Route path='/admin' element={<AdminIndex />}>
-          <Route path='/admin' element={<AdminMain />} />
+        <Route path="/" element={<ClientIndex />} />
+        <Route path="/login" element={<MemberIndex value="login" />} />
+        <Route path="/join" element={<MemberIndex value="join" />} />
+		
+        <Route path="/admin" element={<AdminIndex />}>
+          <Route path="/admin" element={<AdminMain />} />
           <Route path='/admin/member' element={<AdminMember />}>
             {/* 경로설정 */}
           </Route>
@@ -35,7 +37,7 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
