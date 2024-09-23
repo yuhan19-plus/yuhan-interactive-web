50ee63252f2692bb5cda5539946a4ff0eb121159
Seong-Jun1525 <sjsj123455@naver.com>
2024-09-23 22:38:43 +0900

    컴소과 학과장 캐릭터 수정 및 WelcomeZone 병합 전 최종완성 - 성준

--- frontend/src/client/components/canvas/maps/player/dept/CSDeptHeadCharacter.jsx
+++ frontend/src/client/components/canvas/maps/player/dept/CSDeptHeadCharacter.jsx
@@ -10,22 +10,20 @@ export function CSDeptHeadCharacter({position, ...props}) {
   const { nodes, materials } = useGraph(clone)
   const { actions } = useAnimations(animations, group)
 
-  // console.log(animations)
+   // 현재 실행 중인 애니메이션 상태를 저장하는 상태 변수
+   const [animation, setAnimation] = useState('Laugh')
 
-  // 현재 실행 중인 애니메이션 상태를 저장하는 상태 변수
-  const [animation, setAnimation] = useState('Laugh')
-
-  useEffect(() => {
-    if (actions && actions[animation]) {
-      actions[animation].reset().fadeIn(0.3).play()
-    }
-    
-    return () => {
-      if (actions && actions[animation]) {
-        actions[animation].fadeOut(0.3)
-      }
-    }
-  }, [actions, animation])
+   useEffect(() => {
+     if (actions && actions[animation]) {
+       actions[animation].reset().fadeIn(0.3).play()
+     }
+     
+     return () => {
+       if (actions && actions[animation]) {
+         actions[animation].fadeOut(0.3)
+       }
+     }
+   }, [actions, animation])
 
   return (
     <group ref={group} position={position} {...props}>
@@ -35,14 +33,16 @@ export function CSDeptHeadCharacter({position, ...props}) {
           <primitive object={nodes.Thigh_L} />
           <primitive object={nodes.Thigh_R} />
           <primitive object={nodes.neutral_bone} />
-          <skinnedMesh name="Hair" geometry={nodes.Hair.geometry} material={materials.Black} skeleton={nodes.Hair.skeleton} />
           <skinnedMesh name="Jacket" geometry={nodes.Jacket.geometry} material={materials.Material} skeleton={nodes.Jacket.skeleton} />
-          <skinnedMesh name="Pants" geometry={nodes.Pants.geometry} material={materials.Gray} skeleton={nodes.Pants.skeleton} />
           <group name="SD_Join">
             <skinnedMesh name="Cube067" geometry={nodes.Cube067.geometry} material={materials.Body} skeleton={nodes.Cube067.skeleton} />
             <skinnedMesh name="Cube067_1" geometry={nodes.Cube067_1.geometry} material={materials.Black} skeleton={nodes.Cube067_1.skeleton} />
           </group>
-          <skinnedMesh name="Shirt" geometry={nodes.Shirt.geometry} material={materials.Wine} skeleton={nodes.Shirt.skeleton} />
+          <group name="Shirt">
+            <skinnedMesh name="Cube001" geometry={nodes.Cube001.geometry} material={materials.Wine} skeleton={nodes.Cube001.skeleton} />
+            <skinnedMesh name="Cube001_1" geometry={nodes.Cube001_1.geometry} material={materials.Gray} skeleton={nodes.Cube001_1.skeleton} />
+            <skinnedMesh name="Cube001_2" geometry={nodes.Cube001_2.geometry} material={materials.Black} skeleton={nodes.Cube001_2.skeleton} />
+          </group>
           <skinnedMesh name="Shoes" geometry={nodes.Shoes.geometry} material={materials.Black} skeleton={nodes.Shoes.skeleton} />
         </group>
       </group>
