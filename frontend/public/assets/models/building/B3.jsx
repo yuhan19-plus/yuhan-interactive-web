20b0592ce3406101ef468df337e008367c2e220c
Jungmin828 <batman6675@gmail.com>
2024-10-29 21:37:39 +0900

    건물 모델링 수정

---
+++ frontend/public/assets/models/building/B3.jsx
@@ -0,0 +1,41 @@
+/** 파일생성자 : 이정민
+ * 초기 position, scale 설정 : 이정민
+ * position, scale 수정 및 그림자 설정 : 임성준
+ */
+
+import { useGLTF } from '@react-three/drei'
+import React from 'react'
+
+export function Model(props) {
+  //Jayugwan
+  const { scene, nodes, materials } = useGLTF('/assets/models/building/B3.glb')
+  const [meshRef, api] = useBox(() => ({
+    args: [90, 215, 155],
+    type: 'Static',
+    mass: 1,
+    position,
+    ...props
+  }))
+
+  return (
+
+      <group position={[-104.435, 72.171, -147.283]} rotation={[Math.PI / 2, 0, 0]} scale={[8.072, 8.072, 9.321]}>
+        <mesh geometry={nodes.텍스트002.geometry} material={materials['DCE759 & Metal (DEVName, Statue).001']} />
+        <mesh geometry={nodes.텍스트002_1.geometry} material={materials['000000 (B1, B2, B9, Smoking Booth).001']} />
+        <mesh geometry={nodes.텍스트002_2.geometry} material={materials['E2EBCA (B1,B4,B5,B6(BodyFrame)).001']} />
+        <mesh geometry={nodes.텍스트002_3.geometry} material={materials['C0E8F6 (B1~9(Window)).001']} />
+        <mesh geometry={nodes.텍스트002_4.geometry} material={materials['18226AA (Number Plane).001']} />
+        <mesh geometry={nodes.텍스트002_5.geometry} material={materials['FFFFFF (Number, BaskitBall).001']} />
+        <mesh geometry={nodes.텍스트002_6.geometry} material={materials['61544D (B1,B2,B5,B6(Body).001']} />
+        <mesh geometry={nodes.텍스트002_7.geometry} material={materials['23683C (RoofTop).001']} />
+        <mesh geometry={nodes.텍스트002_8.geometry} material={materials['A2A2A2(B9, Park(Rock),B5-6 Plane)']} />
+        <mesh geometry={nodes.텍스트002_9.geometry} material={materials['0EA6EF (Leaf, Smoking Booth)']} />
+        <mesh geometry={nodes.텍스트002_10.geometry} material={materials['644F1C (Park Plane)']} />
+        <mesh geometry={nodes.텍스트002_11.geometry} material={materials['0CFF0F(Leaf,Smoking Booth)']} />
+        <mesh geometry={nodes.텍스트002_12.geometry} material={materials['006400 (Leaf)']} />
+        <mesh geometry={nodes.텍스트002_13.geometry} material={materials['654321 (Wood)']} />
+      </group>
+  )
+}
+
+useGLTF.preload('/B3.glb')
