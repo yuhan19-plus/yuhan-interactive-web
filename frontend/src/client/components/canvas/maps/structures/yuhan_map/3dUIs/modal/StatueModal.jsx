99eba31d311ecf5d7e246045d846ad195b15f94b
Seong-Jun1525 <sjsj123455@naver.com>
2024-10-29 13:08:37 +0900

    동상 모달 스타일 컴포넌트로 변경 - 성준

--- frontend/src/client/components/canvas/maps/structures/yuhan_map/3dUIs/modal/StatueModal.jsx
+++ frontend/src/client/components/canvas/maps/structures/yuhan_map/3dUIs/modal/StatueModal.jsx
@@ -8,10 +8,11 @@
  * - 스타일 컴포넌트로 변경 후 코드 정리
  * **********************************************************
  */
-import { Grid, Typography } from "@mui/material";
 import { Html } from '@react-three/drei';
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDoorClosed, faDoorOpen } from '@fortawesome/free-solid-svg-icons';
 
 const StatueModal = ({ position }) => {
     return (
@@ -19,77 +20,145 @@ const StatueModal = ({ position }) => {
             position={position}
             center
         >
-            <BoardLayout
+            <StatueWrapper
                 onPointerUp={(e) => {
                     e.stopPropagation()
                 }}
             >
-                <Grid sx={{ background: "white", width: '22vw', height: '45.5vh', margin: 1, boxShadow: 2, padding: 1.2, borderRadius: 2 }} >
-                    <Grid sx={{ height: '6.5vh', marginBottom: 1, boxShadow: 2, padding: 1, borderRadius: 2 }}>
-                        <Typography sx={{ fontSize: '14px', fontWeight: 'bold', }}>설립자 유일한,</Typography>
-                        <Typography sx={{ fontSize: '20px', fontWeight: 'bold', color: 'blueviolet' }}>유일한은 누구인가!</Typography>
-                    </Grid >
-                    <Typography>
-                        <p style={{ lineHeight: "19px" }}>
-                            <strong style={{ color: "#04b45f" }}>유일한 박사</strong>는 새로운 기업 윤리를 이 땅에 남긴<br />
+                <StatueContainer>
+                    <StatueHeader>
+                        <p>설립자 유일한, <StatueTitle>유일한은 누구인가!</StatueTitle></p>
+                    </StatueHeader>
+                    <StatueContent>
+                        <p>
+                            <strong style={{ color: "#04b45f" }}>유일한 박사</strong>는 새로운 기업 윤리를 이 땅에 남긴
                             <strong style={{ color: "#04b45f" }}>모범적인 기업가</strong>이며,
                             <strong style={{ color: "green" }}>교육자</strong>이며, 
-                            <strong style={{ color: "#FFD700" }}>사회사업가</strong>이며,<br /> 
-                            그리고 <strong style={{ color: "red" }}>독립운동가</strong>였다.<br />
-                            이러한 그의 공적을 높이 평가하여<br /> 
-                            1971년의 서거 시에 국가 최고의
-                            영예인<br /> 
-                            <strong style={{ color: "violet" }}>「국민훈장 무궁화장」</strong>이 추서되었으며, <br />
-                            1995년 광복 제50주년 경축식에서<br /> 
-                            <strong style={{ color: "#04b45f" }}>「건국훈장 독립장」</strong>이 추서되었고, <br />
-                            1996년에는 문화체육부로부터<br /> 
-                            <strong style={{ color: "orange" }}>「6월의 문화인물」</strong>로 선정되었다.<br />
-                            유일한 박사는 일생의 대부분을 일제하에서 <strong style={{ color: "#04b45f" }}><br />
-                            민족 기업을 설립</strong>하고<br />
-                            발전시키는 데에 바쳤다.<br />
-                            그러나 그와 못지않게 미국에서의 <strong style={{ color: "red" }}>독립투쟁</strong>을 통해<br />
-                            조국의 독립을 위해 노력했던 그의 <strong style={{ color: "blue" }}>애국적인 생활</strong>도<br />
-                            그의 일생에 중요한 일부분으로 높게 평가되고 있다.<br />
+                            <strong style={{ color: "#FFD700" }}>사회사업가</strong>이며,
+                            그리고 <strong style={{ color: "red" }}>독립운동가</strong>였다.
+                            이러한 그의 공적을 높이 평가하여 1971년의 서거 시에 국가 최고의
+                            영예인 <strong style={{ color: "violet" }}>「국민훈장 무궁화장」</strong>이 추서되었으며,
+                            1995년 광복 제50주년 경축식에서 <strong style={{ color: "#04b45f" }}>「건국훈장 독립장」</strong>이 추서되었고,
+                            1996년에는 문화체육부로부터
+                            <strong style={{ color: "orange" }}>「6월의 문화인물」</strong>로 선정되었다.
                         </p>
-                    </Typography>
-                    <button
-                        style={{
-                            width: "21vw",
-                            height: "3.5vh",
-                            marginTop: 8,
-                            color: "white",
-                            background: "#0F275C",
-                            borderRadius: 12
-                        }}>
-                        <a href="https://newih.yuhan.ac.kr/index.do" target="_blank">유일한 박사 온라인 기념관</a>
-                    </button>
-                </Grid>
+                        <p>
+                            유일한 박사는 일생의 대부분을 일제하에서 <strong style={{ color: "#04b45f" }}>
+                            민족 기업을 설립</strong>하고
+                            발전시키는 데에 바쳤다.
+                            그러나 그와 못지않게 미국에서의 <strong style={{ color: "red" }}>독립투쟁</strong>을 통해
+                            조국의 독립을 위해 노력했던 그의 <strong style={{ color: "blue" }}>애국적인 생활</strong>도
+                            그의 일생에 중요한 일부분으로 높게 평가되고 있다.
+                        </p>
+                        <a href="https://newih.yuhan.ac.kr/index.do" target="_blank">
+                            <StatueButton>
+                                <p>유일한 박사 온라인 기념관으로</p>
+                                <FontAwesomeIcon icon={faDoorOpen} />
+                            </StatueButton>
+                        </a>
+                    </StatueContent>
+                </StatueContainer>
 
-                <Grid sx={{
-                    background: "white",
-                    display: 'flex',
-                    width: '12vw',
-                    height: '24vh',
-                    padding: '5px',
-                    justifyContent: 'center',
-                    boxShadow: 2,
-                    marginTop: 1,
-                    borderRadius: 2,
-                }}>
-                    <img style={{ boxShadow: 1, borderRadius: 7, width: "100%", height: "100%" }} src="assets/images/Yu_ilhan.png" />
-                </Grid>
-            </BoardLayout>
+                <PhotoContainer>
+                    <PhotoContent src="assets/images/Yu_ilhan.png" />
+                </PhotoContainer>
+            </StatueWrapper>
         </Html>
     );
 };
 
 export default StatueModal;
 
-const BoardLayout = styled.div`
-  display: flex;
-  justify-content: center;
-  overflow: hidden;
-  width: 66vh;
-  height: 50vh;
-  background: rgba(0,0,0,0);
-`;
+const StatueWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 500px;
+    height: 350px;
+`
+
+const StatueContainer = styled.div`
+    width: 70%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 25px;
+    margin-right: 15px;
+    padding: 10px;
+    background: white;
+`
+
+const StatueHeader = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    /* background-color: #333; */
+    box-shadow: 2px;
+    padding: 15px;
+    border-radius: 25px;
+
+    p {
+        font-size: 24px;
+        font-weight: 900;
+    }
+`
+
+const StatueTitle = styled.p`
+    font-size: 20px;
+    font-weight: 900;
+    color: #0F275C;
+`
+
+const StatueContent = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 5px;
+    font-size: 16px;
+    p {
+        margin-bottom: 7px;
+    }
+`
+
+const StatueButton = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 10px;
+    color: white;
+    padding: 5px;
+    background: #0F275C;
+    border-radius: 12px;
+
+    P {
+        margin: 0px;
+        padding: 0px;
+    }
+
+    svg {
+        margin-left: 10px;
+    }
+`
+
+const PhotoContainer = styled.div`
+    width: 30%;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    height: 40%;
+    padding: 5px;
+    box-shadow: 2px;
+    margin-top: 10px;
+    border-radius: 15px;
+`
+
+const PhotoContent = styled.img`
+    box-shadow: 1px;
+    border-radius: 7px;
+    width: 100%;
+    height: 100%;
+`
